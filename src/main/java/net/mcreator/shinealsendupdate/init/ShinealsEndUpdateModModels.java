df182b7af3dde2a212200c1e2329c4c17fe287aa
GlitchedMastery <teapot635@gmail.com>
2022-02-24 07:53:54 +0000

    MCreator Merge commit '900f7dac5b2f9d701bb98822996088552684eab4' into 46402e5ed537b494ef006ccb1c3a141e2c39cdc4

--- src/main/java/net/mcreator/shinealsendupdate/init/ShinealsEndUpdateModModels.java
+++ src/main/java/net/mcreator/shinealsendupdate/init/ShinealsEndUpdateModModels.java
@@ -9,6 +9,8 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.api.distmarker.Dist;
 
+import net.mcreator.shinealsendupdate.client.model.Modelcustom_model;
+
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = {Dist.CLIENT})
 public class ShinealsEndUpdateModModels {
 	@SubscribeEvent

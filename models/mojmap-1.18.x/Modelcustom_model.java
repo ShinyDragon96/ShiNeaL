65d07d40f7f9bf9e0315b0c04bda0415aa366484
GlitchedMastery <teapot635@gmail.com>
2022-04-09 08:44:25 +0100

    Create Modelcustom_model.java

---
+++ models/mojmap-1.18.x/Modelcustom_model.java
@@ -0,0 +1,45 @@
+// Made with Blockbench 4.2.2
+// Exported for Minecraft version 1.17 with Mojang mappings
+// Paste this class into your mod and generate all required imports
+
+public class Modelcustom_model<T extends Entity> extends EntityModel<T> {
+	// This layer location should be baked with EntityRendererProvider.Context in
+	// the entity renderer and passed into this model's constructor
+	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(
+			new ResourceLocation("modid", "custom_model"), "main");
+	private final ModelPart bb_main;
+
+	public Modelcustom_model(ModelPart root) {
+		this.bb_main = root.getChild("bb_main");
+	}
+
+	public static LayerDefinition createBodyLayer() {
+		MeshDefinition meshdefinition = new MeshDefinition();
+		PartDefinition partdefinition = meshdefinition.getRoot();
+
+		PartDefinition bb_main = partdefinition.addOrReplaceChild("bb_main",
+				CubeListBuilder.create().texOffs(0, 0)
+						.addBox(-4.0F, -6.0F, -9.0F, 8.0F, 6.0F, 18.0F, new CubeDeformation(0.0F)).texOffs(0, 12)
+						.addBox(2.0F, -2.0F, -11.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)).texOffs(8, 0)
+						.addBox(-4.0F, -2.0F, -11.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)).texOffs(6, 6)
+						.addBox(2.0F, -12.0F, -9.0F, 2.0F, 6.0F, 2.0F, new CubeDeformation(0.0F)).texOffs(0, 0)
+						.addBox(-4.0F, -12.0F, -9.0F, 2.0F, 6.0F, 2.0F, new CubeDeformation(0.0F)).texOffs(0, 24)
+						.addBox(-4.5F, -15.0F, -4.0F, 9.0F, 14.0F, 14.0F, new CubeDeformation(0.0F)).texOffs(32, 24)
+						.addBox(-4.0F, -4.0F, 9.0F, 8.0F, 4.0F, 6.0F, new CubeDeformation(0.0F)).texOffs(38, 44)
+						.addBox(4.5F, -10.0F, 1.0F, 2.0F, 8.0F, 8.0F, new CubeDeformation(0.0F)).texOffs(34, 0)
+						.addBox(-6.5F, -10.0F, 1.0F, 2.0F, 8.0F, 8.0F, new CubeDeformation(0.0F)),
+				PartPose.offset(0.0F, 24.0F, 0.0F));
+
+		return LayerDefinition.create(meshdefinition, 64, 64);
+	}
+
+	@Override
+	public void renderToBuffer(PoseStack poseStack, VertexConsumer buffer, int packedLight, int packedOverlay,
+			float red, float green, float blue, float alpha) {
+		bb_main.render(poseStack, buffer, packedLight, packedOverlay);
+	}
+
+	public void setupAnim(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw,
+			float headPitch) {
+	}
+}

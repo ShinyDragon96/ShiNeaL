c58b1d114456cbc61d1c20e38f5492f3e744e7f3
GlitchedMastery <teapot635@gmail.com>
2023-03-29 19:28:54 +0100

    Update stuff

--- src/main/java/net/mcreator/shinealsendupdate/client/model/Modelcustom_model.java
+++ src/main/java/net/mcreator/shinealsendupdate/client/model/Modelcustom_model.java
@@ -15,7 +15,7 @@ import net.minecraft.client.model.EntityModel;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.blaze3d.vertex.PoseStack;
 
-// Made with Blockbench 4.1.5
+// Made with Blockbench 4.2.2
 // Exported for Minecraft version 1.17 with Mojang mappings
 // Paste this class into your mod and generate all required imports
 public class Modelcustom_model<T extends Entity> extends EntityModel<T> {
@@ -23,80 +23,35 @@ public class Modelcustom_model<T extends Entity> extends EntityModel<T> {
 	// the entity renderer and passed into this model's constructor
 	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("shineals_end_update", "modelcustom_model"),
 			"main");
-	public final ModelPart body;
-	public final ModelPart left_leg;
-	public final ModelPart right_leg;
-	public final ModelPart hair_left_middle;
-	public final ModelPart hair_left_bottom;
-	public final ModelPart hair_right_middle;
-	public final ModelPart hair_right_bottom;
+	public final ModelPart bb_main;
 
 	public Modelcustom_model(ModelPart root) {
-		this.body = root.getChild("body");
-		this.left_leg = root.getChild("left_leg");
-		this.right_leg = root.getChild("right_leg");
-		this.hair_left_middle = root.getChild("hair_left_middle");
-		this.hair_left_bottom = root.getChild("hair_left_bottom");
-		this.hair_right_middle = root.getChild("hair_right_middle");
-		this.hair_right_bottom = root.getChild("hair_right_bottom");
+		this.bb_main = root.getChild("bb_main");
 	}
 
 	public static LayerDefinition createBodyLayer() {
 		MeshDefinition meshdefinition = new MeshDefinition();
 		PartDefinition partdefinition = meshdefinition.getRoot();
-		PartDefinition body = partdefinition.addOrReplaceChild(
-				"body", CubeListBuilder.create().texOffs(0, 0).addBox(-8.0F, -6.0F, -8.0F, 16.0F, 14.0F, 16.0F, new CubeDeformation(0.0F))
-						.texOffs(0, 114).addBox(-5.0F, -1.0F, -14.0F, 10.0F, 8.0F, 6.0F, new CubeDeformation(0.0F)),
-				PartPose.offset(0.0F, 1.0F, 0.0F));
-		PartDefinition bone = body.addOrReplaceChild("bone",
-				CubeListBuilder.create().texOffs(4, 33).mirror().addBox(-20.0F, -32.0F, -8.0F, 12.0F, 0.0F, 16.0F, new CubeDeformation(0.0F))
-						.mirror(false).texOffs(4, 33).mirror().addBox(-20.0F, -44.0F, -8.0F, 12.0F, 0.0F, 16.0F, new CubeDeformation(0.0F))
-						.mirror(false),
-				PartPose.offsetAndRotation(-38.0F, 2.0F, 0.0F, 0.0F, 0.0F, 1.5708F));
-		PartDefinition left_leg = partdefinition.addOrReplaceChild("left_leg",
-				CubeListBuilder.create().texOffs(0, 55).addBox(-10.0F, 0.0F, -2.0F, 4.0F, 17.0F, 4.0F, new CubeDeformation(0.0F)),
-				PartPose.offset(4.0F, 7.0F, 0.0F));
-		PartDefinition right_leg = partdefinition.addOrReplaceChild("right_leg",
-				CubeListBuilder.create().texOffs(0, 32).addBox(6.0F, 0.0F, -2.0F, 4.0F, 17.0F, 4.0F, new CubeDeformation(0.0F)),
-				PartPose.offset(-4.0F, 7.0F, 0.0F));
-		PartDefinition hair_left_middle = partdefinition.addOrReplaceChild("hair_left_middle", CubeListBuilder.create(),
-				PartPose.offset(-8.0F, -1.0F, -8.0F));
-		PartDefinition hair_left_middle_rotation = hair_left_middle.addOrReplaceChild("hair_left_middle_rotation",
-				CubeListBuilder.create().texOffs(4, 49).addBox(0.0F, 0.0F, -8.0F, 12.0F, 0.0F, 16.0F, new CubeDeformation(0.0F)),
-				PartPose.offsetAndRotation(16.0F, 3.0F, 8.0F, 0.0F, 0.0F, -1.0472F));
-		PartDefinition hair_left_bottom = partdefinition.addOrReplaceChild("hair_left_bottom", CubeListBuilder.create(),
-				PartPose.offset(-8.0F, 4.0F, -8.0F));
-		PartDefinition hair_left_bottom_rotation = hair_left_bottom.addOrReplaceChild("hair_left_bottom_rotation",
-				CubeListBuilder.create().texOffs(4, 65).addBox(0.0F, 0.0F, -8.0F, 12.0F, 0.0F, 16.0F, new CubeDeformation(0.0F)),
-				PartPose.offsetAndRotation(16.0F, 0.0F, 8.0F, 0.0F, 0.0F, 1.0472F));
-		PartDefinition hair_right_middle = partdefinition.addOrReplaceChild("hair_right_middle", CubeListBuilder.create(),
-				PartPose.offset(8.0F, 0.0F, -8.0F));
-		PartDefinition hair_right_middle_rotation = hair_right_middle.addOrReplaceChild(
-				"hair_right_middle_rotation", CubeListBuilder.create().texOffs(4, 49).mirror()
-						.addBox(-12.0F, 0.0F, -8.0F, 12.0F, 0.0F, 16.0F, new CubeDeformation(0.0F)).mirror(false),
-				PartPose.offsetAndRotation(-16.0F, 3.0F, 8.0F, 0.0F, 0.0F, 1.0472F));
-		PartDefinition hair_right_bottom = partdefinition.addOrReplaceChild("hair_right_bottom", CubeListBuilder.create(),
-				PartPose.offset(8.0F, 5.0F, -8.0F));
-		PartDefinition hair_right_bottom_rotation = hair_right_bottom.addOrReplaceChild(
-				"hair_right_bottom_rotation", CubeListBuilder.create().texOffs(4, 65).mirror()
-						.addBox(-12.0F, 0.0F, -8.0F, 12.0F, 0.0F, 16.0F, new CubeDeformation(0.0F)).mirror(false),
-				PartPose.offsetAndRotation(-16.0F, 0.0F, 8.0F, 0.0F, 0.0F, -1.0472F));
-		return LayerDefinition.create(meshdefinition, 64, 128);
-	}
-
-	@Override
-	public void setupAnim(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+		PartDefinition bb_main = partdefinition.addOrReplaceChild("bb_main",
+				CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -6.0F, -9.0F, 8.0F, 6.0F, 18.0F, new CubeDeformation(0.0F)).texOffs(0, 12)
+						.addBox(2.0F, -2.0F, -11.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)).texOffs(8, 0)
+						.addBox(-4.0F, -2.0F, -11.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)).texOffs(6, 6)
+						.addBox(2.0F, -12.0F, -9.0F, 2.0F, 6.0F, 2.0F, new CubeDeformation(0.0F)).texOffs(0, 0)
+						.addBox(-4.0F, -12.0F, -9.0F, 2.0F, 6.0F, 2.0F, new CubeDeformation(0.0F)).texOffs(0, 24)
+						.addBox(-4.5F, -15.0F, -4.0F, 9.0F, 14.0F, 14.0F, new CubeDeformation(0.0F)).texOffs(32, 24)
+						.addBox(-4.0F, -4.0F, 9.0F, 8.0F, 4.0F, 6.0F, new CubeDeformation(0.0F)).texOffs(38, 44)
+						.addBox(4.5F, -10.0F, 1.0F, 2.0F, 8.0F, 8.0F, new CubeDeformation(0.0F)).texOffs(34, 0)
+						.addBox(-6.5F, -10.0F, 1.0F, 2.0F, 8.0F, 8.0F, new CubeDeformation(0.0F)),
+				PartPose.offset(0.0F, 24.0F, 0.0F));
+		return LayerDefinition.create(meshdefinition, 64, 64);
 	}
 
 	@Override
 	public void renderToBuffer(PoseStack poseStack, VertexConsumer buffer, int packedLight, int packedOverlay, float red, float green, float blue,
 			float alpha) {
-		body.render(poseStack, buffer, packedLight, packedOverlay);
-		left_leg.render(poseStack, buffer, packedLight, packedOverlay);
-		right_leg.render(poseStack, buffer, packedLight, packedOverlay);
-		hair_left_middle.render(poseStack, buffer, packedLight, packedOverlay);
-		hair_left_bottom.render(poseStack, buffer, packedLight, packedOverlay);
-		hair_right_middle.render(poseStack, buffer, packedLight, packedOverlay);
-		hair_right_bottom.render(poseStack, buffer, packedLight, packedOverlay);
+		bb_main.render(poseStack, buffer, packedLight, packedOverlay);
+	}
+
+	public void setupAnim(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
 	}
 }
